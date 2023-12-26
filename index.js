971afb875afcabee531cd8f4afdc07d8dc2195c3
harshit0571 <harshitchadha65@gmail.com>
2023-12-26 20:17:33 +0530

    mongo db connected

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-import express from "express";
+const express = require("express");
+const dotenv = require("dotenv");
+const { connectToDB } = require("./utils/database");
+dotenv.config();
 
 const app = express();
+
+connectToDB();
+
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
-app.listen(port, () => {
-  console.log("app running ");
+app.listen(9000, () => {
+  console.log("app running on port, " + port);
 });
---
+++ utils/database.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+let isConnected = false;
+
+const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+  if (isConnected) {
+    console.log("is connected");
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: "true",
+      useUnifiedTopology: "true",
+    });
+    console.log("db connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { connectToDB };
