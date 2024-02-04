edeb718a6de634d11678b08aa2d0995963be363d
harshit0571 <harshitchadha65@gmail.com>
2024-02-04 12:56:18 +0530

    progress schema added

--- models/User.js
+++ models/User.js
@@ -1,18 +1,27 @@
 const mongoose = require("mongoose");
 
+const courseProgressSchema = new mongoose.Schema({
+  courseId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Course",
+    required: true,
+  },
+  progress: [
+    {
+      type: Number,
+      default: 0,
+      min: 0,
+      max: 100,
+    },
+  ],
+});
+
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true },
   name: String,
   email: { type: String, unique: true },
   password: String,
-  selectedCourses: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Course",
-    },
-  ],
-  //progress
-  
+  selectedCourses: [courseProgressSchema],
 });
 
 const User = mongoose.model("User", userSchema);
